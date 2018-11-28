526333be01b85e58ee67db492d438c122439b342
Cody Burleson <cody.burleson@base22.com>
2018-11-27 19:11:17 -0700

    Implement load blog post from Carbon by slug

--- src/components/blog-detail/index.js
+++ src/components/blog-detail/index.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import {CarbonLDP} from "carbonldp/CarbonLDP";
+
+let carbonldp = new CarbonLDP("http://localhost:8083");
 
 class BlogDetail extends Component {
 
-    componentDidMount() {
-        const { slug } = this.props.match.params;
-        console.log('Slug: %o',slug)
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: {
+                $id: '',
+                title: '',
+                slug: '',
+                excerpt: '',
+                body: ''
+            }
+        };
     }
-    render() {
 
+    componentDidMount() {
+        const {slug} = this.props.match.params;
+        carbonldp.documents.$get("posts/" + slug + "/")
+            .then((persistedPost) => {
+                this.setState({post: persistedPost});
+            })
+            .catch(error => console.error(error));
+    }
+
+    render() {
+        let post = this.state.post;
         return (
             <div className="Home">
                 <h1>Blog Detail</h1>
+                <h2>{post.title}</h2>
+                <p>ID: {post.$id}</p>
+                <p>Slug: {post.slug}</p>
+                <p>Excerpt: {post.excerpt}</p>
+                <hr/>
+                {post.body}
             </div>
         );
     }
